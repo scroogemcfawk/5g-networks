8a5a34a476f6ed04ebc54023d688b0f2bc69bc74
Stepan Bylkov <scroogemcfawk@gmail.com>
2024-10-14 14:31:33 +0700

    add lab 3

--- src/generator/Point.kt
+++ src/generator/Point.kt
@@ -1,7 +1,6 @@
 package generator
 
-import kotlin.math.pow
-import kotlin.math.sqrt
+import kotlin.math.*
 
 /**
  * Stepan Bylkov (smf)<br/>
@@ -61,14 +60,73 @@ data class Adjustment(
     val x: Double,
     val y: Double
 ) {
-
     constructor(x: Number, y: Number) : this(x.toDouble(), y.toDouble())
 }
 
+private const val TWO_PI = 2 * PI
+
+data class Sector(
+    val pivot: Point,
+    val direction: Double,
+    val fov: Double
+) {
+    private val boundUpper = direction + fov / 2
+    private val boundLower = direction - fov / 2
+
+    fun getDirectionEndPoint(distance: Number): Point {
+        return pointFromPivotWithAngle(distance, direction)
+    }
+
+    private fun pointFromPivotWithAngle(distance: Number, directionAngle: Double): Point {
+        val x = cos(directionAngle) * distance.toDouble()
+        val y = sin(directionAngle) * distance.toDouble()
+        return Point(pivot.x + x, pivot.y + y)
+    }
+
+    fun contains(p: Point): Boolean {
+        println("Sector: $this")
+        println("Other: $p")
+
+        val adjustment = pivot.getAdjustment(p)
+        val angle = atan2(adjustment.y, adjustment.x).let {
+            if (it < 0) it + TWO_PI else
+                if (it > TWO_PI) it - TWO_PI else it
+        }
+        println("Angle: ${degrees(angle)}")
+        return angle in boundLower..boundUpper
+    }
+
+    fun getLineEndsInSquare(distance: Number): Pair<Point, Point> {
+        return Pair(
+            pointFromPivotWithAngle(distance, boundUpper),
+            pointFromPivotWithAngle(distance, boundLower)
+        )
+    }
+
+    override fun toString(): String {
+        return "Sector(pivot=$pivot, direction=${degrees(direction)}, fov=${degrees(fov)}, bu=${degrees(boundUpper)}, bl=${degrees(boundLower)})"
+    }
+
+    private fun degrees(angle: Number): Int {
+        return ((angle.toDouble() / TWO_PI) * 360).toInt()
+    }
+}
+
 fun main() {
-    val a = Point(0, 0)
-    val b = Point(1, 1)
-    for (p in a.interpolate(b, 10)) {
-        println(p)
+    val f = Point(0, 0)
+
+    val t0 = Point(1, 0)
+    val t45 = Point(1, 1)
+    val t90 = Point(0, 1)
+    val t135 = Point(-1, 1)
+    val t180 = Point(-1, 0)
+    val t225 = Point(-1, -1)
+    val t270 = Point(0, -1)
+    val t315 = Point(1, -1)
+
+    val s = Sector(f, PI / 2, PI)
+    println(s)
+    for (p in listOf(t0, t45, t90, t135, t180, t225, t270, t315)) {
+        println("$p ${s.contains(p)}")
     }
 }
